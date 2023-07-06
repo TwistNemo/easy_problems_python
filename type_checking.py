1430faf8c21e88c62d3d48fe9233dfc57d49c740
TwistNemo <user.email>
2023-07-06 12:12:58 +0600

    first commit21

---
+++ type_checking.py
@@ -0,0 +1,41 @@
+# Numeric Types:
+n = 123  # int
+print(type(n))  # Output: <class 'int'>
+
+n = 1.123  # float
+print(type(n))  # Output: <class 'float'>
+
+n = 1+2j  # complex
+print(type(n))  # Output: <class 'complex'>
+
+# Sequence Types:
+my_list = [1, 2, 3]  # list
+print(type(my_list))  # Output: <class 'list'>
+
+my_tuple = (1, 2, 3)  # tuple
+print(type(my_tuple))  # Output: <class 'tuple'>
+
+my_str = "Hello"  # str
+print(type(my_str))  # Output: <class 'str'>
+
+# Mapping Type:
+my_dict = {"key": "value"}  # dict
+print(type(my_dict))  # Output: <class 'dict'>
+
+# Set Types:
+my_set = {1, 2, 3}  # set
+print(type(my_set))  # Output: <class 'set'>
+
+my_frozenset = frozenset({1, 2, 3})  # frozenset
+print(type(my_frozenset))  # Output: <class 'frozenset'>
+
+# Boolean Type:
+b = True  # bool
+print(type(b))  # Output: <class 'bool'>
+
+b = False  # bool
+print(type(b))  # Output: <class 'bool'>
+
+# None Type:
+my_var = None  # NoneType
+print(type(my_var))  # Output: <class 'NoneType'>
